72b59216be02e55f6a037616cfa3245c3fb9a15d
EnjoyCode <enjoycode@icloud.com>
2023-12-18 08:57:01 +0800

    [Dynamic.Design] 先简单实现一下Color的属性编辑器.

--- src/PixUI.Dynamic.Design/PropertyEditors/ColorEditor.cs
+++ src/PixUI.Dynamic.Design/PropertyEditors/ColorEditor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Globalization;
 
 namespace PixUI.Dynamic.Design;
@@ -8,28 +9,21 @@ public sealed class ColorEditor : ValueEditorBase
 
     public ColorEditor(State<Color?> color, DesignElement element) : base(element)
     {
-        _color = color;
-        if (color.Value.HasValue)
-            _colorHex.Value = ((uint)color.Value.Value).ToString("X2");
-        _colorHex.AddListener(OnColorHexChanged);
+        var colorHex = color.ToComputed(
+            s => s.HasValue ? ((uint)s.Value).ToString("X2") : string.Empty,
+            v =>
+            {
+                if (uint.TryParse(v, NumberStyles.HexNumber, null, out var value))
+                    color.Value = value;
+            });
 
         var colorState = color.ToNoneNullable(Colors.Transparent);
         //var iconState = color.ToComputed(v => v.HasValue ? MaterialIcons.Square : MaterialIcons.ColorLens);
         //Child = new Button(icon: iconState) { TextColor = colorState, Style = ButtonStyle.Outline, OnTap = OnTap };
 
-        Child = new TextInput(_colorHex)
+        Child = new TextInput(colorHex)
         {
-            Prefix = new Icon(MaterialIcons.Square) { Color = colorState }
+            Suffix = new Icon(MaterialIcons.Square) { Color = colorState }
         };
-    }
-
-    private readonly State<Color?> _color;
-    private readonly State<string> _colorHex = string.Empty;
-
-    private void OnColorHexChanged(State state)
-    {
-        if (!uint.TryParse(_colorHex.Value, NumberStyles.HexNumber, null, out var value))
-            return;
-        _color.Value = value;
     }
 }
