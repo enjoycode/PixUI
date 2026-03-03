90aa275d0cd0b70297f4284050c872d573b68666
EnjoyCode <enjoycode@icloud.com>
2026-03-03 10:56:50 +0800

    [Mac] fix mac app can't copy or paste to other application.

--- src/PixUI.Platform.Mac/MacClipboard.cs
+++ src/PixUI.Platform.Mac/MacClipboard.cs
@@ -5,18 +5,20 @@ namespace PixUI.Platform.Mac;
 
 internal sealed class MacClipboard : IPlatformClipboard
 {
+    private const string StringDataType = "public.utf8-plain-text";
+
     public ValueTask WriteText(string text)
     {
         var pasteboard = NSPasteboard.GeneralPasteboard;
         pasteboard.ClearContents(); // Empty the current contents
-        pasteboard.SetStringForType(text, "string");
+        pasteboard.SetStringForType(text, StringDataType);
         return new ValueTask();
     }
 
     public ValueTask<string?> ReadText()
     {
         var pasteboard = NSPasteboard.GeneralPasteboard;
-        var text = pasteboard.GetStringForType("string");
+        var text = pasteboard.GetStringForType(StringDataType);
         return new ValueTask<string?>(text);
     }
 }
