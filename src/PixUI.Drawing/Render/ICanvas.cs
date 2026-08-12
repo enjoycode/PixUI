a69ae84b1cc48e19b498c7e1f3cefc43efb71836
EnjoyCode <enjoycode@icloud.com>
2026-08-12 10:08:56 +0800

    初步实现画Chart

--- src/PixUI.Drawing/Render/ICanvas.cs
+++ src/PixUI.Drawing/Render/ICanvas.cs
@@ -26,6 +26,7 @@ public interface ICanvas : IDisposable
     void DrawImage(IImage image, Rect dest, IPaint? paint = null);
     void DrawImage(IImage image, Rect source, Rect dest, IPaint? paint = null);
     void DrawPicture(IPicture picture, float x, float y, IPaint? paint = null);
+    void DrawPicture(IPicture picture, in Matrix3 matrix, IPaint? paint = null);
     void DrawShadow(IPath path, Color color, float elevation, bool transparentOccluder, float devicePixelRatio);
 
     #endregion
